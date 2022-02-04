e4286c89b3f7f54c0a02265795d21b552b4eb84b
sahaj <sahajjain13@gmail.com>
2022-02-05 01:17:35 +0530

    Initial Setup

---
+++ src/_tests_/sample.test.js
@@ -0,0 +1,6 @@
+/* eslint-env jest */
+import sum from '../index';
+
+it('sample', () => {
+  expect(sum(1, 5)).toBe(6);
+});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('working');
+
+const sum = (a, b) => {
+  return a + b;
+}
+
+export default sum;
